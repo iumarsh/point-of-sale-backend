04b814e088a6a5776ed312c45bc5505f6ed1f99c
Umar Sheikh <sheikhgarts@gmail.com>
2026-06-30 21:54:45 +0500

    fixed the cors issue

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const cors = require("cors");
 
 const routes = require("./routes/index.js");
 const connectDB = require("./db/connect");
- 
 
 require("dotenv").config();
 const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://point-of-sale-react.vercel.app",
+    credentials: true,
+  }),
+);
 
 app.use(routes);
 const port = process.env.PORT || 5000;
 
-const CONNECTION_URL = process.env.CONNECTION_URL
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 const start = async () => {
   try {
